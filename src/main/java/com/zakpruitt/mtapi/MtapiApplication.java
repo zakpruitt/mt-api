7863d08dad11567254edd91a68f5fd91ef25002b
Zak Pruitt <pruittzn@gmail.com>
2021-08-16 17:25:52 -0500

    init

---
+++ src/main/java/com/zakpruitt/mtapi/MtapiApplication.java
@@ -0,0 +1,13 @@
+package com.zakpruitt.mtapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zakpruitt/mtapi/MtapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zakpruitt.mtapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
