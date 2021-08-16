77452f1a29653ac6f0d4187d74845cadb938359a
Zak Pruitt <pruittzn@gmail.com>
2021-08-16 17:30:10 -0500

    init

--- src/main/java/com/zakpruitt/mtapi/MtapiApplication.java
+++ src/main/java/com/zakpruitt/mtapi/MtapiApplication.java
@@ -2,12 +2,20 @@ package com.zakpruitt.mtapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class MtapiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MtapiApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
 	}
 
 }
