6abc49ce1e81882dbfdb2a3092e90d202b707e41
Zak Pruitt <pruittzn@gmail.com>
2021-08-16 19:02:47 -0500

    card, spell, and creature class

--- src/main/java/com/zakpruitt/mtapi/domain/Card.java
+++ src/main/java/com/zakpruitt/mtapi/domain/Card.java
@@ -1,5 +1,40 @@
 package com.zakpruitt.mtapi.domain;
 
 
-public class Card {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.lang.Nullable;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Data
+@Entity
+public class Card extends AuditModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+
+    @NotEmpty(message = "Card Name is required.")
+    private String cardName;
+    @NotEmpty(message = "Card Lore is required.")
+    private String cardLore;
+    @NotNull
+    private String type;
+    @NotNull
+    private String rarity;
+    @NotNull
+    private String clan;
+    @Nullable
+    private List<String> cardEffects;
+    @NotNull
+    private String cardDescription;
+    @NotNull
+    private int emberCost;
+    @NotNull
+    private String imageURL;
 }
---
+++ src/main/java/com/zakpruitt/mtapi/domain/Spell.java
@@ -0,0 +1,16 @@
+package com.zakpruitt.mtapi.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table
+public class Spell {
+}
