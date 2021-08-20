e0a3452b046f68fad68853cbbc3a61b46253b0ed
Zak Pruitt <pruittzn@gmail.com>
2021-08-20 00:47:01 -0500

    swagger documentation

--- src/main/java/com/zakpruitt/mtapi/MtapiApplication.java
+++ src/main/java/com/zakpruitt/mtapi/MtapiApplication.java
@@ -2,8 +2,6 @@ package com.zakpruitt.mtapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class MtapiApplication {
