8017e69e8f49bee8fdfe922c2a4daa8395b55eb7
Zak Pruitt <pruittzn@gmail.com>
2021-08-20 23:58:33 -0500

    artifact class decorated

--- src/main/java/com/zakpruitt/mtapi/domain/Artifact/Artifact.java
+++ src/main/java/com/zakpruitt/mtapi/domain/Artifact/Artifact.java
@@ -1,4 +1,26 @@
 package com.zakpruitt.mtapi.domain.Artifact;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Artifact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty(message = "Card Name is required.")
+    private String artifactName;
+    @NotEmpty(message = "Card Lore is required.")
+    private String artifactDescription;
+    @NotEmpty(message = "Card Lore is required.")
+    @Lob
+    private String artifactLore;
 }
