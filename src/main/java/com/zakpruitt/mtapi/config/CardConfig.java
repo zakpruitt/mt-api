4ddcab3f3d66b7218c50a44f74a9ae9a12889799
Zak Pruitt <pruittzn@gmail.com>
2021-09-02 17:42:37 -0500

    regex reconfigure and switch statement bug fix

--- src/main/java/com/zakpruitt/mtapi/config/CardConfig.java
+++ src/main/java/com/zakpruitt/mtapi/config/CardConfig.java
@@ -52,9 +52,8 @@ public class CardConfig {
                 JSONArray cardArray = cardJSON.getJSONArray("cards");
                 for(int j = 0; j < cardArray.length(); j++){
                     JSONObject card = cardArray.getJSONObject(j);
-                    Pattern regex = Pattern.compile("\\\"(.*?)\\\"", Pattern.MULTILINE);
-                    Matcher matcher = regex.matcher(card.getString("description"));
-
+                    String description = parseDescription(card.getString("description"));
+                    System.out.println(description);
 //                    if (card.getString("cardType") == "Monster") {
 //                        CreatureCard newCard = new CreatureCard();
 //                        newCard.setCardName(card.getString("name"));
@@ -77,42 +76,41 @@ public class CardConfig {
 
     private String parseDescription(String description) {
         if (description.equals("")) return description;
-
         if (description.contains("<sprite")) {
             description = parseSpriteTags(description);
         }
         description = parseEscapeCharacters(description);
-
+        description = parseHTMLTags(description);
 
         return description;
     }
 
     private String parseSpriteTags(String description) {
-        Pattern regex = Pattern.compile("\\\"(.*?)\\\"", Pattern.MULTILINE);
+        Pattern regex = Pattern.compile("\\\"(.*?)\\\"");
         Matcher regexMatcher = regex.matcher(description);
         while (regexMatcher.find()) {
             for (int i = 0; i < regexMatcher.groupCount(); i++) {
-                switch (regexMatcher.group(i)) {
+                switch (regexMatcher.group(i).replaceAll("\"", "")) {
                     case "Attack":
-                        description = description.replace("<sprite name=\\\"Attack\\\">", "Attack");
+                        description = description.replace("<sprite name=\"Attack\">", "Attack");
                         break;
                     case "Health":
-                        description = description.replace("<sprite name=\\\"Health\\\">", "Health");
+                        description = description.replace("<sprite name=\"Health\">", "Health");
                         break;
                     case "ChargedEchoes":
-                        description = description.replace("<sprite name=\\\"ChargedEchoes\\\">", "ChargedEchoes");
+                        description = description.replace("<sprite name=\"ChargedEchoes\">", "ChargedEchoes");
                         break;
                     case "Xcost":
-                        description = description.replace("<sprite name=\\\"Xcost\\\">", "Xcost");
+                        description = description.replace("<sprite name=\"Xcost\">", "Xcost");
                         break;
                     case "Ember":
-                        description = description.replace("<sprite name=\\\"Ember\\\">", "Ember");
+                        description = description.replace("<sprite name=\"Ember\">", "Ember");
                         break;
                     case "Gold":
-                        description = description.replace("<sprite name=\\\"Gold\\\">", "Gold");
+                        description = description.replace("<sprite name=\"Gold\">", "Gold");
                         break;
                     case "Capacity":
-                        description = description.replace("<sprite name=\\\"Capacity\\\">", "Capacity");
+                        description = description.replace("<sprite name=\"Capacity\">", "Capacity");
                         break;
                 }
             }
@@ -121,20 +119,10 @@ public class CardConfig {
     }
 
     private String parseEscapeCharacters(String description) {
-        Pattern regex = Pattern.compile("(\\\\.)+", Pattern.MULTILINE);
-        Matcher regexMatcher = regex.matcher(description);
-        if (regexMatcher.find()) {
-            regexMatcher.replaceAll("");
-        }
-        return description;
+        return description.replaceAll("(\\\\.)+", "");
     }
 
     public String parseHTMLTags(String description) {
-        Pattern regex = Pattern.compile("<.+?>", Pattern.MULTILINE);
-        Matcher regexMatcher = regex.matcher(description);
-        if (regexMatcher.find()) {
-            regexMatcher.replaceAll("");
-        }
-        return description;
+        return description.replaceAll("<.+?>", "");
     }
 }
