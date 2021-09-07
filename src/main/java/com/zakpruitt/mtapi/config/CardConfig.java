7f51d0c262c927a009f8cdd641b7cb531ecc798c
Zak Pruitt <pruittzn@gmail.com>
2021-09-07 12:54:54 -0500

    added in production handling for card population

--- src/main/java/com/zakpruitt/mtapi/config/CardConfig.java
+++ src/main/java/com/zakpruitt/mtapi/config/CardConfig.java
@@ -6,6 +6,7 @@ import com.zakpruitt.mtapi.repository.SpellCardRepository;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -20,6 +21,9 @@ import java.util.regex.Pattern;
 @Configuration
 public class CardConfig {
 
+    @Value("$MTAPI.ENV")
+    private String env;
+
     @Autowired
     CreatureCardRepository creatureCardRepository;
     @Autowired
@@ -28,54 +32,56 @@ public class CardConfig {
     @Bean
     CommandLineRunner commandLineRunner(CreatureCardRepository creatureCardRepository, SpellCardRepository spellCardRepository) {
         return args -> {
-            for (int i = 1; i < 30; i++) {
-                // Create URL and request connection
-                URL url = new URL(String.format("https://ocffhwpt3b.execute-api.us-west-2.amazonaws.com/production/api/v1/cards?offset=%s&limit=10", i));
-                HttpURLConnection con = (HttpURLConnection) url.openConnection();
-                con.setRequestProperty("Content-Type", "application/json");
-                con.setRequestProperty("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzA2ODIzMjksInBsYXRmb3JtIjoiUHVibGljIiwicGxhdGZvcm1Vc2VySWQiOiJQdWJsaWMiLCJzZXNzaW9uVGlja2V0IjpudWxsLCJyb3V0ZXMiOlsiY2FyZHMiLCJjaGFsbGVuZ2UiLCJjaGFsbGVuZ2VzIiwiY2xhbnMiLCJjb3ZlbmFudHMiLCJhcnRpZmFjdHMiLCJlbmVtaWVzIiwibGVhZGVyYm9hcmQiLCJtdXRhdG9ycyIsInNoYXJlcyIsInNpbnMiLCJwYXRjaG5vdGVzIl0sImJ1aWxkVmVyc2lvbiI6bnVsbH0.KCcAqDtNa7wW59AqHiXZSG6hms_gSIyVd4qktoFCub8");
-                con.setRequestMethod("GET");
+            if (env.equals("dev")) {
+                for (int i = 1; i < 30; i++) {
+                    // Create URL and request connection
+                    URL url = new URL(String.format("https://ocffhwpt3b.execute-api.us-west-2.amazonaws.com/production/api/v1/cards?offset=%s&limit=10", i));
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                    con.setRequestProperty("Content-Type", "application/json");
+                    con.setRequestProperty("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzA2ODIzMjksInBsYXRmb3JtIjoiUHVibGljIiwicGxhdGZvcm1Vc2VySWQiOiJQdWJsaWMiLCJzZXNzaW9uVGlja2V0IjpudWxsLCJyb3V0ZXMiOlsiY2FyZHMiLCJjaGFsbGVuZ2UiLCJjaGFsbGVuZ2VzIiwiY2xhbnMiLCJjb3ZlbmFudHMiLCJhcnRpZmFjdHMiLCJlbmVtaWVzIiwibGVhZGVyYm9hcmQiLCJtdXRhdG9ycyIsInNoYXJlcyIsInNpbnMiLCJwYXRjaG5vdGVzIl0sImJ1aWxkVmVyc2lvbiI6bnVsbH0.KCcAqDtNa7wW59AqHiXZSG6hms_gSIyVd4qktoFCub8");
+                    con.setRequestMethod("GET");
 
-                // Read response
-                int status = con.getResponseCode();
-                BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                String inputLine;
-                StringBuffer content = new StringBuffer();
-                while ((inputLine = in.readLine()) != null) {
-                    content.append(inputLine);
-                }
-                in.close();
-
-                // Parse to JSON
-                JSONObject cardJSON = new JSONObject(content.toString());
-
-                // Iterate over and create card objects
-                JSONArray cardArray = cardJSON.getJSONArray("cards");
-                for(int j = 0; j < cardArray.length(); j++){
-                    JSONObject card = cardArray.getJSONObject(j);
-                    if (creatureCardRepository.findByCardName(card.getString("name")) != null) continue;
-                    if (spellCardRepository.findByCardName(card.getString("name")) != null ) continue;
-
-                    switch (card.getString("cardType")) {
-                        case "Monster":
-                            CreatureCard newCard = new CreatureCard();
-                            newCard.setCardName(card.getString("name"));
-                            newCard.setCardLore(card.getString("lore"));
-                            newCard.setEmberCost(card.getInt("cost"));
-                            newCard.setSubtype(card.getString("cardSubType"));
-                            newCard.setRarity(card.getInt("rarity"));
-                            newCard.setHealth(card.getInt("health"));
-                            newCard.setDamage(card.getInt("attack"));
-                            newCard.setImageURL(card.getString("imageUrl"));
-                            newCard.setType(card.getString("cardType"));
-                            newCard.setDescription(parseDescription(card.getString("description")));
-                            creatureCardRepository.save(newCard);
-                            break;
-                        case "Spell":
-                            break;
+                    // Read response
+                    int status = con.getResponseCode();
+                    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                    String inputLine;
+                    StringBuffer content = new StringBuffer();
+                    while ((inputLine = in.readLine()) != null) {
+                        content.append(inputLine);
                     }
+                    in.close();
+
+                    // Parse to JSON
+                    JSONObject cardJSON = new JSONObject(content.toString());
+
+                    // Iterate over and create card objects
+                    JSONArray cardArray = cardJSON.getJSONArray("cards");
+                    for (int j = 0; j < cardArray.length(); j++) {
+                        JSONObject card = cardArray.getJSONObject(j);
+                        if (creatureCardRepository.findByCardName(card.getString("name")) != null) continue;
+                        if (spellCardRepository.findByCardName(card.getString("name")) != null) continue;
+
+                        switch (card.getString("cardType")) {
+                            case "Monster":
+                                CreatureCard newCard = new CreatureCard();
+                                newCard.setCardName(card.getString("name"));
+                                newCard.setCardLore(card.getString("lore"));
+                                newCard.setEmberCost(card.getInt("cost"));
+                                newCard.setSubtype(card.getString("cardSubType"));
+                                newCard.setRarity(card.getInt("rarity"));
+                                newCard.setHealth(card.getInt("health"));
+                                newCard.setDamage(card.getInt("attack"));
+                                newCard.setImageURL(card.getString("imageUrl"));
+                                newCard.setType(card.getString("cardType"));
+                                newCard.setDescription(parseDescription(card.getString("description")));
+                                creatureCardRepository.save(newCard);
+                                break;
+                            case "Spell":
+                                break;
+                        }
+                    }
+                    con.disconnect();
                 }
-                con.disconnect();
             }
         };
     }
