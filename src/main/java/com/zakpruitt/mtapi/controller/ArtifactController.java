a306db63d3f0fdac831d3c853ef39d3c7556b57b
Zak Pruitt <pruittzn@gmail.com>
2021-10-04 03:13:54 -0500

    get all artifact test

--- src/main/java/com/zakpruitt/mtapi/controller/ArtifactController.java
+++ src/main/java/com/zakpruitt/mtapi/controller/ArtifactController.java
@@ -1,9 +1,31 @@
 package com.zakpruitt.mtapi.controller;
 
+import com.zakpruitt.mtapi.domain.Artifact;
+import com.zakpruitt.mtapi.domain.Card.CreatureCard;
+import com.zakpruitt.mtapi.service.ArtifactService;
+import com.zakpruitt.mtapi.service.CreatureCardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping(path = "/api/v1")
 public class ArtifactController {
+
+    @Autowired
+    private ArtifactService artifactService;
+
+    @GetMapping("/artifacts")
+    public Map<String, Artifact> findAllArtifacts() {
+        return artifactService.getArtifacts();
+    }
+
+    @GetMapping("/cards/{name}")
+    public CreatureCard findCreatureCardByName(@PathVariable String name) {
+        return creatureCardService.getCreatureCardByName(name);
+    }
 }
--- src/main/java/com/zakpruitt/mtapi/service/ArtifactService.java
+++ src/main/java/com/zakpruitt/mtapi/service/ArtifactService.java
@@ -1,7 +1,25 @@
 package com.zakpruitt.mtapi.service;
 
+import com.zakpruitt.mtapi.domain.Artifact;
+import com.zakpruitt.mtapi.domain.Card.CreatureCard;
+import com.zakpruitt.mtapi.repository.ArtifactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 @Service
 public class ArtifactService {
+
+    @Autowired
+    ArtifactRepository artifactRepository;
+
+    public Map<String, Artifact> getArtifacts() {
+        Map<String, Artifact> allArtifacts = new HashMap<>();
+        allArtifacts.putAll(artifactRepository.findAll().stream().collect(Collectors.toMap(Artifact::getArtifactName, Function.identity())));
+        return allArtifacts;
+    }
 }
